1629dffd2ec1f631df2e2213312d078db9bc9bfe
magtyn <mtyndall@bu.edu>
2023-12-05 00:06:01 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,27 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+let color = null;
+
+const jsnails = document.querySelector("#js-nails");
+console.log(jsnails);
+
+jsnails.addEventListener('click', (event) => {
+	console.log(event.target.tagName);
+	if (event.target.tagName === "path") {
+	event.target.style.fill = color;
+  }
+});
+
+const jspolish = document.querySelector("#js-polish");
+console.log(jspolish);
+
+jspolish.addEventListener('click', (event) => {
+  if (event.target.tagName === "IMG" || event.target.tagName === "BUTTON") {
+  console.log(event.target.dataset.color);
+  color = event.target.dataset.color;
+  }
+});
+
+
